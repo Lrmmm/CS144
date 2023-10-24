87fbafa110b98a9e60df436c3644502ab6f19976
lrm <1136942787@qq.com>
2023-10-24 21:43:45 +0800

    Complete lab3

--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -3,11 +3,57 @@
 #include "byte_stream.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+class Timer
+{
+private:
+  uint64_t initial_RTO_ms_;
+  uint64_t curr_RTO_ms;
+  size_t time_ms_{0};
+  bool running_ {false};
+public:
+  explicit Timer(uint64_t init_RTO) : initial_RTO_ms_(init_RTO),
+    curr_RTO_ms(init_RTO){}
+  
+  void start()
+  {
+    running_ = true;
+    time_ms_ = 0;
+  }
+
+  void stop() {running_ = false;}
+
+  bool is_running() const {return running_;}
+
+  bool is_expired() const {return running_ && (time_ms_ >= curr_RTO_ms);}
+
+  void tick(size_t const ms_since_last_tick)
+  {
+    if (running_) {
+      time_ms_ += ms_since_last_tick;
+    }
+  }
+  void double_RTO(){curr_RTO_ms *= 2;}
+  void reset_RTO(){curr_RTO_ms = initial_RTO_ms_;}
+};
 
 class TCPSender
 {
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+
+  // My code
+  bool syn_ {false};
+  bool fin_ {false};
+  unsigned retransmit_cnt_{ 0 };
+  uint64_t acked_seqno_ {0};
+  uint64_t next_seqno_ {0};
+  uint64_t window_size_ {1};
+
+  uint64_t outstanding_cnt_{0};
+  std::queue<TCPSenderMessage> outstanding_segments_ {};
+  std::queue<TCPSenderMessage> queued_segments_{};
+
+  Timer timer_{ initial_RTO_ms_};
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
